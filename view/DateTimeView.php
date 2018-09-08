8243aaa0d2ba024f9eb61321417f3c20b7f9d2b5
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-09-08 14:06:14 +0200

    Displaying server time, in progress

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -3,9 +3,20 @@
 class DateTimeView {
 
 
+	/**
+	 * My method of retrieving servertime has been inspired by Shef's answer here:
+	 * https://stackoverflow.com/questions/6621572/how-to-get-date-and-time-from-server
+	 */
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		$time = new DateTime();
+		$timezone = $time->getTimezone()->getName();
+		date_default_timezone_set($timezone);
+		// $currentTime = getdate(); // more info in SO-post
+
+		$serverTime = date('H:i:s - d/m/Y');
+
+		$timeString = "TODO: make server time more than this: $serverTime";
 
 		return '<p>' . $timeString . '</p>';
 	}
