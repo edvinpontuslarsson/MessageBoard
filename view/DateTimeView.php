52104e34086a8d594ffe3f6f44be5abffcf6fa78
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-09-08 16:11:49 +0200

    Breaks DateTimeView into smaller functions

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -2,22 +2,31 @@
 
 class DateTimeView {
 
+	public function show() {
+
+		$timeString = $this->getTimeString();
+
+		return '<p>' . $timeString . '</p>';
+	}
 
 	/**
 	 * My method of retrieving servertime has been inspired by Shef's answer here:
 	 * https://stackoverflow.com/questions/6621572/how-to-get-date-and-time-from-server
 	 */
-	public function show() {
+	private function getTimeString() {
 
-		$time = new DateTime();
-		$timezone = $time->getTimezone()->getName();
-		date_default_timezone_set($timezone);
+		$this->setDefaultTimezone();
+
 		// $currentTime = getdate(); // more info in SO-post.
 
 		$serverTime = date('H:i:s - d/m/Y');
 
-		$timeString = "TODO: make server time more than this: $serverTime";
+		return "TODO: make server time more than this: $serverTime";
+	}
 
-		return '<p>' . $timeString . '</p>';
+	private function setDefaultTimezone() {
+		$time = new DateTime();
+		$timezone = $time->getTimezone()->getName();
+		date_default_timezone_set($timezone);
 	}
 }
