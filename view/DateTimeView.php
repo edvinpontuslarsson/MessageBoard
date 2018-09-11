dbb298f733e7c857a24da464ebda2882ae30edd4
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-09-11 21:40:58 +0200

    git ignores environment.php

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -13,13 +13,13 @@ class DateTimeView {
 
 		$this->setDefaultTimezone();
 
-		$time = date('H:i:s');
-
 		$dateArray = getdate();
 		$weekday = $dateArray['weekday'];
 		$mday = $dateArray['mday'];
 		$month = $dateArray['month'];
 		$year = $dateArray['year'];
+
+		$time = date('H:i:s');
 
 		return "$weekday, the {$mday}th of $month $year, The time is $time";
 	}
