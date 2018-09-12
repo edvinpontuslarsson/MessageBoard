1ae6aac189d91ad07e45a61c9bbf673c35af0c8f
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-09-12 08:56:35 +0200

    Adds skeleton for DbConnection class in model, failed attempt at getting the time from separate class

--- model/DateTime.php
+++ model/DateTime.php
@@ -1,26 +1,19 @@
 <?php
 
-// require_once('../model/DateTime.php');
+class DateTime {
+	
+    public function getDateTimeString() {
 
-class DateTimeView {
-
-	public function show() {
-		$timeString = $this->getDateTimeString();
-		return '<p>' . $timeString . '</p>';
-	}
-
-	/*
-		TODO: remove below, require from model/DateTime.php
-	*/
-
-	private function getDateTimeString() {
 		$this->setDefaultTimezone();
+
 		$dateArray = getdate();
 		$weekday = $dateArray['weekday'];
 		$mday = $dateArray['mday'];
 		$month = $dateArray['month'];
 		$year = $dateArray['year'];
+
 		$time = date('H:i:s');
+
 		return "$weekday, the {$mday}th of $month $year, The time is $time";
 	}
 
---
+++ model/DbConnection.php
@@ -0,0 +1,8 @@
+<?php
+
+class DbConnection {
+    
+    public function connect() {
+
+    }
+}
