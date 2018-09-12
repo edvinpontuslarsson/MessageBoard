a7ab1be4137d43d101d1f52e045af881b2900a52
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-09-12 11:07:22 +0200

    gets timeString from separate class but in same file now

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -2,41 +2,20 @@
 
 // require_once('../model/DateTime.php');
 
-class DateTime {
-	public function getHello() {
-		$hello = $this->createHello();
-		return $hello;
-	}
+class DateTimeModel {
+	
+    public function getDateTimeString() {
 
-	private function createHello() {
-		return 'Hello';
-	}
-}
-
-class DateTimeView {
-
-	public function show() {
-		// $timeString = $this->getDateTimeString();
-
-		$greeting = new DateTime();
-
-		$timeString = $greeting->getHello();
-
-		return '<p>' . $timeString . '</p>';
-	}
-
-	/*
-		TODO: remove below, require from model/DateTime.php
-	*/
-
-	private function getDateTimeString() {
 		$this->setDefaultTimezone();
+
 		$dateArray = getdate();
 		$weekday = $dateArray['weekday'];
 		$mday = $dateArray['mday'];
 		$month = $dateArray['month'];
 		$year = $dateArray['year'];
+
 		$time = date('H:i:s');
+
 		return "$weekday, the {$mday}th of $month $year, The time is $time";
 	}
 
@@ -44,5 +23,16 @@ class DateTimeView {
 		$time = new DateTime();
 		$timezone = $time->getTimezone()->getName();
 		date_default_timezone_set($timezone);
+	}
+}
+
+class DateTimeView {
+
+	public function show() {
+		$dateTimeModel = new DateTimeModel();
+
+		$timeString = $dateTimeModel->getDateTimeString();
+
+		return '<p>' . $timeString . '</p>';
 	}
 }
