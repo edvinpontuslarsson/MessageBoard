dd5acfcea9f46cd421a8672a99af87709b0f67d9
Edvin Larsson <edvinpontuslarsson@gmail.com>
2018-09-14 11:59:44 +0200

    Adding some empty funtions to UserModel

--- model/UserModel.php
+++ model/UserModel.php
@@ -2,7 +2,20 @@
 
 class UserModel {
 
-    public function __construct() {
-        
+    private $userName;
+    private $rawPassword;
+    private $userId;
+
+    public function __construct($userName, $rawPassword) {
+        $this->userName = $userName;
+        $this->rawPassword = $rawPassword;
     }
+
+    public function storeNewUser() {
+
+    }
+
+    public function authenticateUser() {}
+
+    
 }
